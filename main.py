89cb47df4d41557b260a5d8ab0d5c5e73d89e47d
rmacapobre <romeo.macapobre@gmail.com>
2017-07-08 01:08:09 +0800

    ajoute class Person ajoute darthSidious

--- main.py
+++ main.py
@@ -1,4 +1,51 @@
 import pygame
+import glob
+
+class Person:
+    def __init__(self, startx, starty, name, screen):
+        self.x = startx
+        self.y = starty
+        self.current = 1
+        self.direction = pygame.K_DOWN
+
+        # Get list of filenames
+        self.frontNames = sorted(glob.glob("images/" + name + "Front*.png"))
+        self.backNames = sorted(glob.glob("images/" + name + "Back*.png"))
+        self.leftNames = sorted(glob.glob("images/" + name + "Left*.png"))
+        self.rightNames = sorted(glob.glob("images/" + name + "Right*.png"))
+
+        # load images
+        self.front = []
+        self.back = []
+        self.left = []
+        self.right = []
+        for item in self.frontNames: self.front.append(pygame.image.load(item))
+        for item in self.backNames: self.back.append(pygame.image.load(item))
+        for item in self.leftNames: self.left.append(pygame.image.load(item))
+        for item in self.rightNames: self.right.append(pygame.image.load(item))
+
+    def display(self, screen):
+        pos = (self.x, self.y)
+        if (self.direction == pygame.K_DOWN or self.direction == pygame.K_SPACE):
+            if (not self.front): return
+            screen.blit(self.front[self.current-1], pos)
+        if (self.direction == pygame.K_UP):
+            if (not self.back): return
+            screen.blit(self.back[self.current-1], pos)
+        if (self.direction == pygame.K_LEFT):
+            if (not self.left): return
+            screen.blit(self.left[self.current-1], pos)
+        if (self.direction == pygame.K_RIGHT):
+            if (not self.right): return
+            screen.blit(self.right[self.current-1], pos)
+        self.current += 1
+        if (self.current == 5): self.current = 1
+    def move(self):
+        if (self.direction == pygame.K_DOWN): self.y += 5
+        if (self.direction == pygame.K_UP): self.y -= 5
+        if (self.direction == pygame.K_LEFT): self.x -= 5
+        if (self.direction == pygame.K_RIGHT): self.x += 5
+
 
 # Entry point
 
@@ -19,31 +66,9 @@ comicSansMS.set_bold(True)
 mf = pygame.image.load("images/mf.jpg")
 mf = pygame.transform.scale(mf, (500, 400))
 
-leiaFront1 = pygame.image.load("images/leiaFront1.png")
-leiaFront2 = pygame.image.load("images/leiaFront2.png")
-leiaFront3 = pygame.image.load("images/leiaFront3.png")
-leiaFront4 = pygame.image.load("images/leiaFront4.png")
+darthSidius = Person(350, 60, "ds", screen)
+princesseLeia = Person(10, 350, "leia", screen)
 
-leiaLeft1 = pygame.image.load("images/leiaLeft1.png")
-leiaLeft2 = pygame.image.load("images/leiaLeft2.png")
-leiaLeft3 = pygame.image.load("images/leiaLeft3.png")
-leiaLeft4 = pygame.image.load("images/leiaLeft4.png")
-
-leiaRight1 = pygame.image.load("images/leiaRight1.png")
-leiaRight2 = pygame.image.load("images/leiaRight2.png")
-leiaRight3 = pygame.image.load("images/leiaRight3.png")
-leiaRight4 = pygame.image.load("images/leiaRight4.png")
-
-leiaBack1 = pygame.image.load("images/leiaBack1.png")
-leiaBack2 = pygame.image.load("images/leiaBack2.png")
-leiaBack3 = pygame.image.load("images/leiaBack3.png")
-leiaBack4 = pygame.image.load("images/leiaBack4.png")
-
-current = 1
-
-
-x = 10
-y = 10
 done = False
 pygame.mixer.music.load("sounds/femalefootstep.wav")
 direction = pygame.K_DOWN
@@ -62,47 +87,15 @@ while not done:
     screen.fill(black)
     screen.blit(mf, (0, 0))
 
-    msg = "x = " + str(x) + ", y = " + str(y)
+    msg = "x=" + str(princesseLeia.x) + " y=" + str(princesseLeia.y)
     bonjour = comicSansMS.render(msg, False, black)
-    screen.blit(bonjour, (23, 300))
+    screen.blit(bonjour, (23, 10))
 
+    darthSidius.display(screen)
 
-    if (direction == pygame.K_DOWN):
-        if (current == 1): screen.blit(leiaFront1, (x, y))
-        elif (current == 2):
-            screen.blit(leiaFront2, (x, y))
-        elif (current == 3): screen.blit(leiaFront3, (x, y))
-        elif (current == 4):
-            screen.blit(leiaFront4, (x, y))
-        y += 5
-        if (y >= 300): direction = pygame.K_RIGHT
-    elif (direction == pygame.K_UP):
-        if (current == 1): screen.blit(leiaBack1, (x, y))
-        elif (current == 2): screen.blit(leiaBack2, (x, y))
-        elif (current == 3): screen.blit(leiaBack3, (x, y))
-        elif (current == 4): screen.blit(leiaBack4, (x, y))
-        y -= 5
-        if (y <= 10): direction = pygame.K_LEFT
-    elif (direction == pygame.K_LEFT):
-        if (current == 1): screen.blit(leiaLeft1, (x, y))
-        elif (current == 2): screen.blit(leiaLeft2, (x, y))
-        elif (current == 3): screen.blit(leiaLeft3, (x, y))
-        elif (current == 4): screen.blit(leiaLeft4, (x, y))
-        x -= 5
-        if (x <= 10): direction = pygame.K_DOWN
-    elif (direction == pygame.K_RIGHT):
-        if (current == 1): screen.blit(leiaRight1, (x, y))
-        elif (current == 2): screen.blit(leiaRight2, (x, y))
-        elif (current == 3): screen.blit(leiaRight3, (x, y))
-        elif (current == 4): screen.blit(leiaRight4, (x, y))
-        x += 5
-        if (x >= 300): direction = pygame.K_UP
-    else:
-        # Stop walking
-        continue
-
-    current += 1
-    if (current == 5): current = 1
+    princesseLeia.direction = direction
+    princesseLeia.display(screen)
+    princesseLeia.move()
 
     pygame.display.flip()
     clock.tick(60)
